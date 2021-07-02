3d6ff764caf03f5d704a8e295d04abbe214486c5
Xita Rps <xitarps@gmail.com>
2021-07-01 21:24:19 -0300

    Refactor custom serialization for article

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,12 +1,6 @@
 class Api::V1::ArticlesController < Api::V1::CustomApiController
   def index
-    articles = Article.all.map { |article| { type: 'articles',
-                                             id: article.id,
-                                             attributes: {
-                                               title: article.title,
-                                               content: article.content,
-                                               slug: article.slug
-                                             }}}
+    articles = Article.all.map { |article| article.serialize_to_api }
     data = { data: articles }
     render json: data
   end
@@ -15,14 +9,7 @@ class Api::V1::ArticlesController < Api::V1::CustomApiController
 
     article = Article.find(params[:id]) 
 
-    data = { data: 
-              { type: 'articles',
-                id: article.id,
-                attributes: {
-                title: article.title,
-                content: article.content,
-                slug: article.slug
-                }}}
+    data = { data: article.serialize_to_api }
 
     render json: data
   end
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,15 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   validates :slug, presence: true, uniqueness: true
+
+  def serialize_to_api
+    { type: 'articles',
+      id: id,
+      attributes: {
+        title: title,
+        content: content,
+        slug: slug
+      }
+    }
+  end
 end
